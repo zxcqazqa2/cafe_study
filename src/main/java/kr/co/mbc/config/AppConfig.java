b4f43be1accbcd9f71383ecf13ab94b4f7a6b807
장봉준 <zxcqazqa2@naver.com>
2025-02-10 15:57:03 +0900

    환경설정 완료

---
+++ src/main/java/kr/co/mbc/config/AppConfig.java
@@ -0,0 +1,12 @@
+package kr.co.mbc.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+	/*
+	 * @Bean public RestTemplate restTemplate() { return new RestTemplate(); }
+	 */
+	
+}
